ae0cd155833551d83e0de43b5e53ddbe183c6ec9
Parikshit Panwar <parikshit.panwar@wsu.edu>
2023-11-03 19:42:52 -0700

    Implemented footer basic structure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,37 @@
+import { Box, HStack } from "@chakra-ui/react";
+import Logo from '../Assets/Logo.svg';
+
 const Footer = () => {
     return (
-        <foorter></foorter>
+        <footer>
+            <HStack spacing='25px'>
+                <Box w='150px' h='80px'>
+                    <img src={Logo} alt='Logo' />
+                </Box>
+                <ul>
+                    <li>Doormat</li>
+                    <li>Navigation</li>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Reservations</li>
+                    <li>Order Online</li>
+                    <li>Login</li>
+                </ul>
+                <ul>
+                    <li>Contact</li>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+                <ul>
+                    <li>Social Media Links</li>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </HStack>
+        </footer>
     );
 }
 
