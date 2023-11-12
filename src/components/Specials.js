1bc88bd0a9d81585c6120a0cbb82fe7e77394d74
Parikshit Panwar <parikshit.panwar@wsu.edu>
2023-11-11 18:54:59 -0800

    Refactored Specials Component

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -3,51 +3,44 @@ import { Card } from "react-bootstrap";
 import Main from "./Main";
 import Logo from '../Assets/lemon dessert.jpg';
 import Header from "./Header";
-import { WrapItem, flexbox } from "@chakra-ui/react";
-import { wrap } from "framer-motion";
 
 const Specials = () => {
+    const cardMenu = ['Greek Salad', 'Bruchetta', 'Lemon Dessert'];
+    const itemPrice = ['$12.99', '$5.99', '$5.00'];
+    const cardArray = [];
+
+    for (let i = 0; i < cardMenu.length; i++) {
+        cardArray.push(
+            <Card key = {i} style={{ width: 200}}>
+                <Card.Img variant="top" src={Logo} />
+                <Card.Body>
+                    <Card.Title style={{display: 'flex', width: '100%', justifyContent: 'space-between'}}>
+                        <Card.Title style={{fontSize: 20}}>{cardMenu[i]}</Card.Title>
+                        <Card.Text style={{fontSize: 15}}>{itemPrice[i]}</Card.Text>
+                    </Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button className='button-class'>Order a delivery</Button>
+                </Card.Body>
+            </Card>
+        );
+    }
+
     return (
-        <section>
-            <Header classes="flex-container specialsMobile" headerStyles={{gap: 500, marginTop: 50, marginBottom: 30}}>
-                <h1>Specials</h1>
-                <Button className="button-class">Order Menu</Button>
-            </Header>
-            <Main classes='flex-container'>
-                <Card style={{ width: 200}}>
-                    <Card.Img variant="top" src={Logo} />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                        </Card.Text>
-                        <Button className='button-class'>Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: 200}}>
-                    <Card.Img variant="top" src={Logo} />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                        </Card.Text>
-                        <Button className='button-class'>Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: 200}}>
-                    <Card.Img variant="top" src={Logo} />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                        </Card.Text>
-                        <Button className='button-class'>Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </Main>
+        <section role='specials'>
+            <header style={{display: 'flex', width: '100%', justifyContent: 'center'}}>
+                <Header classes="flex-container specialsMobile" headerStyles={{gap: 500, marginTop: 50, marginBottom: 30}}>
+                    <h1>Specials</h1>
+                    <Button className="button-class">Order Menu</Button>
+                </Header>
+            </header>
+            <main style={{display: 'flex', width: '100%', justifyContent: 'center'}}>
+                <Main classes='flex-container' mainStyles={{width: 800}}>
+                    {cardArray}
+                </Main>
+            </main>
         </section>
     );
 }
