19ebfb5110f181b484962e3f8b42289fe5268906
Parikshit Panwar <parikshit.panwar@wsu.edu>
2023-11-11 19:07:43 -0800

    Updated Specials Component

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -5,18 +5,32 @@ import Logo from '../Assets/lemon dessert.jpg';
 import Header from "./Header";
 
 const Specials = () => {
-    const cardMenu = ['Greek Salad', 'Bruchetta', 'Lemon Dessert'];
-    const itemPrice = ['$12.99', '$5.99', '$5.00'];
     const cardArray = [];
+    const specialItems = [
+        {
+            name: 'Greek Salad',
+            price: 12.99
+        },
 
-    for (let i = 0; i < cardMenu.length; i++) {
+        {
+            name: 'Bruchetta',
+            price: 5.99
+        },
+
+        {
+            name: 'Lemon Dessert',
+            price: 5.00
+        }
+    ];
+
+    for (let i = 0; i < specialItems.length; i++) {
         cardArray.push(
             <Card key = {i} style={{ width: 200}}>
                 <Card.Img variant="top" src={Logo} />
                 <Card.Body>
                     <Card.Title style={{display: 'flex', width: '100%', justifyContent: 'space-between'}}>
-                        <Card.Title style={{fontSize: 20}}>{cardMenu[i]}</Card.Title>
-                        <Card.Text style={{fontSize: 15}}>{itemPrice[i]}</Card.Text>
+                        <Card.Title style={{fontSize: 20}}>{specialItems[i].name}</Card.Title>
+                        <Card.Text style={{fontSize: 15}}>${specialItems[i].price.toFixed(2)}</Card.Text>
                     </Card.Title>
                     <Card.Text>
                     Some quick example text to build on the card title and make up the
