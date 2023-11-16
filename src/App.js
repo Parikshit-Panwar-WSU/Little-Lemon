029b34f961f3f811c2be7ba494ada916b3cdb7a9
Parikshit Panwar <parikshit.panwar@wsu.edu>
2023-11-15 18:41:07 -0800

    Starting of Implementation of Reservation Page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import './App.css';
 import React from 'react';
 import Nav from './components/Nav.js';
 import Footer from './components/Footer.js';
-import HeroSection from './components/HeroSection.js';
-import Specials from './components/Specials.js';
-import Testimonials from './components/Testimonials.js';
-import About from './components/About.js';
+import HomePage from './components/HomePage.js';
+import ReservationPage from './components/ReservationPage.js';
 
 function App() {
   return (
     <>
       <Nav />
-      <HeroSection />
-      <Specials />
-      <Testimonials />
-      <About />
+        <ReservationPage />
       <Footer />
     </>
   );
---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import HeroSection from "./HeroSection";
+import Specials from "./Specials";
+import Testimonials from "./Testimonials";
+import About from './About';
+
+const HomePage = () => {
+    return (
+        <>
+            <HeroSection />
+            <Specials />
+            <Testimonials />
+            <About />
+        </>
+    );
+}
+
+export default HomePage;
